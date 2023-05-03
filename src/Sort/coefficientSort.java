4c38572ccc25a54e200fdacfa55050fd9aa040af
jaeku <kjklalala2@naver.com>
2023-05-03 14:05:29 +0900

    계수정렬

---
+++ src/Sort/coefficientSort.java
@@ -0,0 +1,25 @@
+package Sort;
+// 거스름돈
+
+import java.util.Scanner;
+
+public class selectSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = {7,5,9,0,3,1,6,2,9,1,4,8,0,5,2};
+        int max_value = 9;
+
+        int[] count = new int[max_value+1];
+
+        for(int i=0; i<arr.length; i++){
+            count[arr[i]] += 1;
+        }
+
+        for(int i=0; i<=max_value; i++){
+            for(int j=0; j<count[i]; j++){
+                System.out.print(i+ " ");
+            }
+        }
+    }
+}
