cc1442b3fe97169bbbe4789afadb51b27ab3621d
jaeku <kjklalala2@naver.com>
2023-05-04 12:05:17 +0900

    위에서 아래로

---
+++ src/Sort/fromUpToDown.java
@@ -0,0 +1,25 @@
+package Sort;
+
+import java.util.*;
+
+public class selectSort {
+
+    public static void main(String[] args) {
+
+       Scanner input = new Scanner(System.in);
+
+       int n = input.nextInt();
+       int[] arr = new int[n];
+
+       for(int i=0; i<n; i++){
+           arr[i]=input.nextInt();
+       }
+
+       Arrays.sort(arr);
+
+        for(int i=0; i<n; i++){
+            System.out.println(arr[i]+" ");
+        }
+
+    }
+}
